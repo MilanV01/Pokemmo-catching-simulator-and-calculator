2715e583a8cfc5d6cd927f847f21f816d2be3ad6
Milan Vuković <vukovicmilan2001@gmail.com>
2022-08-20 00:21:52 +0200

    Update CatchRate.py

--- CatchRate.py
+++ CatchRate.py
@@ -5,13 +5,50 @@ import Database
 def Input():
     PercentofHP=input("Enter % HP: ")
     Pokemon=input("Pokemon name: ")
-    
-    CatchRate=input("Enter base Catch Rate: ")
+    for ix, item in enumerate(Database.search(Pokemon)):CatchRate=item[3]
+    for ix, item in enumerate(Database.search(Pokemon)):BaseHP=item[4]
+    print(Database.search(Pokemon))
+    SelectedLocation=input("Enter selected location: ")
+    # for ix, item in enumerate(Database.search(Pokemon)):Location=item[7]
+    # odvojeno=Location.splitlines()
+    # list=odvojeno for tkinter combobox
+    #selected value = string selectedlocation
+    start = SelectedLocation.find('Lvl.') + len('Lvl.')
+    LevelRange=SelectedLocation[start:]
+    print(LevelRange)
+    if len(LevelRange)>2:
+        levels=LevelRange.split("-")
+        level=(int(levels[1])+int(levels[0]))/2
+    else:level=LevelRange
+    Rarity=(SelectedLocation.split(" | ")[-2])
     Pokeball=input("Enter base Pokeball Rate: ")
     Status=input("Enter status: ")
-    BaseHP=input("BASEHP: ")
-    level=input("NIVO: ")
+    one=SelectedLocation.find("|")+1
+    two=SelectedLocation.find("|",one)+1
+    end=SelectedLocation.find("|",two)-1
+    EditedStr=SelectedLocation[:end]
+
+    if EditedStr[-1]==")":
+        end=EditedStr.find(" (")
+        EditedStr=EditedStr[:end]
+
+    print(EditedStr)
+    AllLocations=[]
+    for ix, item in enumerate(Database.searchAll(EditedStr,Pokemon)):AllLocations.append(item[7])
+    print(AllLocations)
+    Uncommon=VCommon=Common=Rare=Vrare=0
+    for n in AllLocations:
+        if(EditedStr+" | Uncommon" in n):Uncommon+=1
+        elif(EditedStr+" | Very Common" in n):VCommon+=1
+        elif(EditedStr+" | Common" in n):Common+-1
+        elif(EditedStr+" | Very Rare" in n):Vrare+=1
+        elif(EditedStr+" | Rare" in n):Rare+=1
+
+
+    print("Uncommon: "+str(Uncommon)+" Very common: "+str(VCommon)+" Common: "+str(Common)+" Very Rare: "+str(Vrare)+" Rare: "+str(Rare))
+
     Calculate(BaseHP,level,PercentofHP,CatchRate,Pokeball,Status)
+
 
 def Calculate(BaseHP,level,PercentofHP,CatchRate,Pokeball,Status):
     CatchPercentage=0
@@ -20,7 +57,8 @@ def Calculate(BaseHP,level,PercentofHP,CatchRate,Pokeball,Status):
     print(MaxHp)
     X=int((round(int((round(round(((3*float(MaxHp)-2*float(CurrentHp))*float(CatchRate)*float(Pokeball)) * 4096) / 4096 * 4096) / 4096)/(3*int(MaxHp)) * 4096) / 4096 *float(Status) * 4096) / 4096 )* 4096) / 4096
     if(X>=255):CatchPercentage=100
-    else:CatchPercentage=round((X/255)**0.75,5)  #maybe add rounding to floor x and add the y formula for uber precision
-    print(str(CatchPercentage*100)+"%")
+    else:CatchPercentage=(X/255)**0.75  #maybe add rounding to floor x and add the y formula for extra extra precision
+    if(CatchPercentage*100>=100):print("100%")
+    else:print(str(round(CatchPercentage*100,3))+"%")
 
 Input()
